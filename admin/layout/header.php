563e2e20eaa4ee2c58b665fa48ead26679827b01
xmartina <martinsprince126@gmail.com>
2024-12-06 00:21:42 +0100

    d

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -270,10 +270,13 @@ $adminName = $row['firstname']." ".$row['lastname'];
                     </li>
 
                     <li>
-                        <a href="./funduser.php"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> Credit/Deibt User </a>
+                        <a href="./funduser.php"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> Credit/Debit User </a>
                     </li>
                     <li>
                         <a href="./transfer.php"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> Wire Transfer User </a>
+                    </li>
+                    <li>
+                        <a href="./send_manuel_mail"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg></span> Send Mail </a>
                     </li>
                 </ul>
             </div>
