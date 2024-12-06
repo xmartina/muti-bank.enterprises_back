a60eb2311a51bbb8142936c4f3453dc9db5562a0
xmartina <martinsprince126@gmail.com>
2024-12-06 08:13:55 +0100

    d

--- layout/header.php
+++ layout/header.php
@@ -28,25 +28,25 @@ $sendMail = new emailMessage();
     <link rel="icon" type="image/x-icon" href="/assets/images/logo/favicon.png"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
-    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/css/plugins.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/css/authentication/form-2.css" rel="stylesheet" type="text/css" />
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/css/authentication/form-2.css" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
-    <link rel="stylesheet" type="text/css" href="/assets/css/forms/theme-checkbox-radio.css">
-    <link rel="stylesheet" type="text/css" href="/assets/css/forms/switches.css">
-    <link href="/assets/css/pages/error/style-400.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/forms/theme-checkbox-radio.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/forms/switches.css">
+    <link href="./assets/css/pages/error/style-400.css" rel="stylesheet" type="text/css" />
 
 
     <!-- BEGIN THEME GLOBAL STYLES -->
-    <link href="/assets/css/scrollspyNav.css" rel="stylesheet" type="text/css" />
-    <link href="/plugins/animate/animate.css" rel="stylesheet" type="text/css" />
-    <link href="/plugins/notification/snackbar/snackbar.min.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="/assets/css/elements/alert.css">
-    <script src="/plugins/sweetalerts/promise-polyfill.js"></script>
-    <link href="/plugins/sweetalerts/sweetalert2.min.css" rel="stylesheet" type="text/css" />
-    <link href="/plugins/sweetalerts/sweetalert.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/css/components/custom-sweetalert.css" rel="stylesheet" type="text/css" />
-    <script src="/assets/js/libs/jquery-3.1.1.min.js"></script>
+    <link href="./assets/css/scrollspyNav.css" rel="stylesheet" type="text/css" />
+    <link href="./plugins/animate/animate.css" rel="stylesheet" type="text/css" />
+    <link href="./plugins/notification/snackbar/snackbar.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/elements/alert.css">
+    <script src="./plugins/sweetalerts/promise-polyfill.js"></script>
+    <link href="./plugins/sweetalerts/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+    <link href="./plugins/sweetalerts/sweetalert.css" rel="stylesheet" type="text/css" />
+    <link href="./assets/css/components/custom-sweetalert.css" rel="stylesheet" type="text/css" />
+    <script src="./assets/js/libs/jquery-3.1.1.min.js"></script>
 
     <!-- END THEME GLOBAL STYLES -->
     <title>Pin</title>
